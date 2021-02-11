54f884b3f5f40e85fd9d2bbcda14e8901b30163f
dinosunny <dino.sunny@cabotsolutions.com>
2021-02-11 12:08:35 +0530

    validation added

--- app/src/main/java/com/target/targetcasestudy/data/Validators.kt
+++ app/src/main/java/com/target/targetcasestudy/data/Validators.kt
@@ -1,5 +1,7 @@
 package com.target.targetcasestudy.data
 
+import androidx.core.text.isDigitsOnly
+
 /**
  * For an explanation of how to validate credit card numbers read:
  *
@@ -22,5 +24,34 @@ package com.target.targetcasestudy.data
  * otherwise false
  */
 fun validateCreditCard(creditCardNumber: String): Boolean {
+  //creating a new array with credit card numbers
+  if (creditCardNumber.isNotEmpty()
+          && creditCardNumber.length>= 13 && creditCardNumber.length<=19 && isNumber(creditCardNumber)) {
+    val cardNumbers: MutableList<String> = ArrayList()
+    for (index in creditCardNumber.indices) {
+      cardNumbers.add(creditCardNumber[index].toString())
+    }
+    val arraySize = cardNumbers.size
+    var sum = 0
+    for (i in arraySize - 2 downTo 0) {
+      var currentSum = cardNumbers[i].toInt()
+      val isOdd = (arraySize - 1 - i) % 2
+      if (isOdd != 0) {
+        currentSum *= 2
+      }
+      if (currentSum > 9) {
+        currentSum -= 9
+      }
+      sum += currentSum
+    }
+    return (sum + cardNumbers[arraySize - 1].toInt()) % 10 == 0
+  }
   return false
+}
+
+//check a given string is numeric
+fun isNumber(str: String) = try { str.toDouble()
+  true
+} catch (e: NumberFormatException) {
+  false
 }
