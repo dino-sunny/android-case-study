e56a723e91c9d7e8caff0fcc9f1af16e52f96522
dinosunny <dino.sunny@cabotsolutions.com>
2021-02-11 12:09:46 +0530

    unit test added to validation

--- app/src/test/java/com/target/targetcasestudy/CreditCardValidatorTest.kt
+++ app/src/test/java/com/target/targetcasestudy/CreditCardValidatorTest.kt
@@ -3,6 +3,9 @@ package com.target.targetcasestudy
 import com.target.targetcasestudy.data.validateCreditCard
 import org.junit.Assert
 import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+import junit.framework.Assert.assertEquals
 
 /**
  * Feel free to make modifications to these unit tests! Remember, you have full technical control
@@ -15,5 +18,36 @@ class CreditCardValidatorTest {
       "valid credit card number should yield true",
       validateCreditCard("4539976741512043")
     )
+  }
+}
+@RunWith(Parameterized::class)
+class CreditCardTest(
+        private val expected: Boolean,
+        private val input: String
+) {
+
+  companion object{
+    @JvmStatic
+    @Parameterized.Parameters(name="validateCreditCard")
+    fun nameTests() = listOf(
+            arrayOf(true,"4539976741512043"),
+            arrayOf(true,"0000000000000"),
+            //Maestro card
+            arrayOf(true,"0604449361108540"),
+            //Diners Club - North America
+            arrayOf(true,"5470217205212764"),
+            //VISA
+            arrayOf(true,"4679026900646420"),
+            arrayOf(false,""),
+            arrayOf(false,"000000"),
+            arrayOf(false,"9999999999999"),
+            arrayOf(false,"testingacreditcard"),
+    )
+  }
+
+  @Test
+  fun test_isValidName(){
+    val actual = validateCreditCard(input)
+    assertEquals(expected, actual)
   }
 }
